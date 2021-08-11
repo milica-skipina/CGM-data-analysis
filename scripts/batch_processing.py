26f7a3098ade95d0f3246fdca68be1551b8672d6
milica-skipina <skipinamilica@gmail.com>
2021-08-11 16:03:23 +0200

    regression

--- scripts/batch_processing.py
+++ scripts/batch_processing.py
@@ -13,53 +13,186 @@ def quiet_logs(sc):
 
 
 HDFS_NAMENODE = os.environ["CORE_CONF_fs_defaultFS"]
-
-conf = SparkConf().setAppName("statistics_hour").setMaster("spark://spark-master:7077")
+conf = SparkConf().setAppName("batch_processing").setMaster("spark://spark-master:7077")
 sc = SparkContext(conf=conf)
 spark = SparkSession(sc)
-
 quiet_logs(spark)
 
-read_file = "/processed/HDeviceCGM.csv/Year=*/Month=*"
-df = spark.read.option("sep", "|").option("header", "true").csv(HDFS_NAMENODE + read_file)
-df = df.drop("DexInternalDtTmDaysFromEnroll").drop("DexInternalTm").drop("RecordType")\
+
+print("-------------------- Patients - Screening data --------------------")
+screening_file = "/data/HScreening.txt"
+screening_df = spark.read.option("sep", "|").option("header", "true").csv(HDFS_NAMENODE + screening_file)
+screening_df.createOrReplaceTempView("patients")
+query = "SELECT PtID, Gender, Ethnicity, Race, DiagAge, Weight, Height, CGMUseStatus FROM patients"
+df_patients = spark.sql(query)
+df_patients.show(5, False)
+
+
+print("-------------------- CGM data --------------------")
+cgm_file = "/processed/HDeviceCGM.csv/Year=*/Month=*"
+df_cgm = spark.read.option("sep", "|").option("header", "true").csv(HDFS_NAMENODE + cgm_file)
+df_cgm = df_cgm.drop("DexInternalDtTmDaysFromEnroll").drop("DexInternalTm").drop("RecordType")\
     .withColumn("GlucoseValue", col("GlucoseValue").cast(FloatType()))\
     .withColumn("DateTime", concat(col("Date"), lit(' '), col("Time")))\
     .withColumn("DateTime", to_timestamp(col("DateTime")))\
     .withColumn("Hour", hour("Time"))
-
-
-df.show(5)
-# df = df.withColumn("Year", year(df["Date"])).withColumn("Month", month(df["Date"])).withColumn("Day", dayofmonth(df["Date"]))
+df_cgm.show(5)
 
 # find how many times glucose value could be measured
-# device_not_used = df_cgm.select("PtID", "DateTime", "GlucoseValue")\
-#     .groupBy("PtID")\
-#     .agg(min("DateTime").alias("FirstDateTime"), max("DateTime").alias("LastDateTime"), count("*").alias("Total"))\
-#     .withColumn("MaxPossible", round((col("LastDateTime").cast(LongType()) - col("FirstDateTime").cast(LongType()))/300))\
-#     .withColumn("DeviceNotUsed", col("MaxPossible").cast(LongType()) - col("Total"))
-# device_not_used.show()
-# device_not_used = device_not_used.select("PtID", "DeviceNotUsed")
+device_not_used = df_cgm.select("PtID", "DateTime", "GlucoseValue")\
+    .groupBy("PtID")\
+    .agg(min("DateTime").alias("FirstDateTime"), max("DateTime").alias("LastDateTime"), count("*").alias("Total"))\
+    .withColumn("MaxPossible", round((col("LastDateTime").cast(LongType()) - col("FirstDateTime").cast(LongType()))/300))\
+    .withColumn("DeviceNotUsed", col("MaxPossible").cast(LongType()) - col("Total"))
+device_not_used.show()
+device_not_used = device_not_used.select("PtID", "DeviceNotUsed")
 
-# glucose level severity by hour
-group_very_high = df.filter("GlucoseValue >= 250").groupBy("PtID", "Hour") \
+
+print("-------------------- BGM data --------------------")
+bgm_file = "/processed/HDeviceBGM.csv"
+df_bgm = spark.read.option("sep", "|").option("header", "true").csv(HDFS_NAMENODE + bgm_file)
+df_bgm = df_bgm.withColumn("GlucoseValueBGM", col("GlucoseValue").cast(FloatType())) \
+    .withColumn("RecIDBGM", col("RecID").cast(IntegerType())) \
+    .drop("RecID") \
+    .drop("RecordType")\
+    .drop("GlucoseValue")\
+    .drop("RecordSubType")\
+    .withColumn("DateTimeBGM", concat(col("Date"), lit(' '), col("Time"))) \
+    .withColumn("DateTimeBGM", to_timestamp(col("DateTimeBGM")))\
+    .drop("Time")
+df_bgm.show(5)
+
+
+print("-------------------- CGMUseStatus --------------------")
+cgm_use = df_patients.groupBy("CGMUseStatus").agg(count("*"))
+cgm_use.show()
+
+
+print("-------------------- Race --------------------")
+race = df_patients.groupBy("Race").agg(count("*"))
+race.show()
+
+
+print("-------------------- Ethnicity --------------------")
+ethnicity = df_patients.groupBy("Ethnicity").agg(count("*"))
+ethnicity.show()
+
+
+print("-------------------- Gender --------------------")
+gender = df_patients.groupBy("Gender").agg(count("*"))
+gender.show()
+
+
+print("-------------------- Screening and CGM data --------------------")
+patients_cgm = df_patients.join(df_cgm, "PtID", "inner")
+patients_cgm.show(10)
+
+
+print("-------------------- Gender + Glucose --------------------")
+patients_cgm.createOrReplaceTempView("patient_cgm")
+query = "SELECT Gender, GlucoseValue FROM patient_cgm"
+df_gender_glucose = spark.sql(query)
+df_gender_glucose.show(10)
+gender_glucose_file = "/results/gender_glucose.csv"
+df_gender_glucose.repartition(1).write.mode("overwrite").option("header", "true") \
+    .csv(HDFS_NAMENODE + gender_glucose_file)
+
+
+# statistics
+# values by glucose level severity for grouped by patient and date
+group_very_high = df_cgm.filter("GlucoseValue >= 250").groupBy("PtID", "Date") \
     .agg(count("*").alias("VeryHigh"))
-group_high = df.filter("GlucoseValue < 250 AND GlucoseValue >= 180").groupBy("PtID", "Hour") \
+group_high = df_cgm.filter("GlucoseValue < 250 AND GlucoseValue >= 180").groupBy("PtID", "Date") \
     .agg(count("*").alias("High"))
-group_in_range = df.filter("GlucoseValue < 180 AND GlucoseValue >= 70").groupBy("PtID", "Hour") \
+group_in_range = df_cgm.filter("GlucoseValue < 180 AND GlucoseValue >= 70").groupBy("PtID", "Date") \
     .agg(count("*").alias("InRange"))
-group_low = df.filter("GlucoseValue < 70 AND GlucoseValue >= 54").groupBy("PtID", "Hour") \
+group_low = df_cgm.filter("GlucoseValue < 70 AND GlucoseValue >= 54").groupBy("PtID", "Date") \
     .agg(count("*").alias("Low"))
-group_very_low = df.filter("GlucoseValue < 54").groupBy("PtID", "Hour") \
+group_very_low = df_cgm.filter("GlucoseValue < 54").groupBy("PtID", "Date") \
     .agg(count("*").alias("VeryLow"))
 
-join = group_very_high.join(group_high, ["PtID", "Hour"], "inner"). \
+statistics_df = group_very_high.join(group_high, ["PtID", "Date"], "inner"). \
+    join(group_in_range, ["PtID", "Date"], "inner"). \
+    join(group_low, ["PtID", "Date"], "inner"). \
+    join(group_very_low, ["PtID", "Date"], "inner")
+
+print("Number of measured values by glucose level severity for individual patient and date")
+statistics_df.show(10)
+statistics_file = "/results/statistics.csv"
+statistics_df.repartition(1).write.mode("overwrite").option("header", "true").option("sep", "|").csv(
+    HDFS_NAMENODE + statistics_file)
+
+print("Percentage values for every measured value for every patient")
+all_statistics = statistics_df.groupBy("PtID").agg(
+    sum(col("VeryHigh")).alias("VeryHigh"),
+    sum(col("High")).alias("High"),
+    sum(col("InRange")).alias("InRange"),
+    sum(col("Low")).alias("Low"),
+    sum(col("VeryLow")).alias("VeryLow"),
+)
+all_statistics = all_statistics.join(device_not_used, ["PtID"], "inner")
+result = all_statistics.withColumn("sum", all_statistics["VeryHigh"].cast(IntegerType())
+                                   + all_statistics["High"].cast(IntegerType())
+                                   + all_statistics["InRange"].cast(IntegerType())
+                                   + all_statistics["Low"].cast(IntegerType())
+                                   + all_statistics["VeryLow"].cast(IntegerType())
+                                   + all_statistics["DeviceNotUsed"].cast(IntegerType()))
+result = result.withColumn("VeryHigh", 100*result["VeryHigh"].cast(FloatType())/result["sum"].cast(IntegerType()))\
+    .withColumn("High", 100*result["High"].cast(FloatType())/result["sum"].cast(IntegerType()))\
+    .withColumn("InRange", 100*result["InRange"].cast(FloatType())/result["sum"].cast(IntegerType()))\
+    .withColumn("Low", 100*result["Low"].cast(FloatType())/result["sum"].cast(IntegerType()))\
+    .withColumn("VeryLow", 100*result["VeryLow"].cast(FloatType())/result["sum"].cast(IntegerType())) \
+    .withColumn("DeviceNotUsed", 100 * result["DeviceNotUsed"].cast(FloatType()) / result["sum"].cast(IntegerType()))
+result = result.drop("sum")
+result.show(10)
+
+all_stat_file = "/results/statistics_percentage.csv"
+result.repartition(1).write.mode("overwrite").option("head", "ture").csv(HDFS_NAMENODE + all_stat_file)
+
+
+# glucose level severity by hour
+group_very_high = df_cgm.filter("GlucoseValue >= 250").groupBy("PtID", "Hour") \
+    .agg(count("*").alias("VeryHigh"))
+group_high = df_cgm.filter("GlucoseValue < 250 AND GlucoseValue >= 180").groupBy("PtID", "Hour") \
+    .agg(count("*").alias("High"))
+group_in_range = df_cgm.filter("GlucoseValue < 180 AND GlucoseValue >= 70").groupBy("PtID", "Hour") \
+    .agg(count("*").alias("InRange"))
+group_low = df_cgm.filter("GlucoseValue < 70 AND GlucoseValue >= 54").groupBy("PtID", "Hour") \
+    .agg(count("*").alias("Low"))
+group_very_low = df_cgm.filter("GlucoseValue < 54").groupBy("PtID", "Hour") \
+    .agg(count("*").alias("VeryLow"))
+
+
+print("-------------------- Glucose level severity by hour --------------------")
+glucose_by_hour = group_very_high.join(group_high, ["PtID", "Hour"], "inner"). \
     join(group_in_range, ["PtID", "Hour"], "inner"). \
     join(group_low, ["PtID", "Hour"], "inner"). \
     join(group_very_low, ["PtID", "Hour"], "inner")
+glucose_by_hour.show(10)
+glucose_by_hour_file = "/results/severity_by_hour.csv"
+glucose_by_hour.repartition(1).write.mode("overwrite").option("header", "true").option("sep", "|").csv(
+    HDFS_NAMENODE + glucose_by_hour_file)
 
-print("Glucose level severity by hour")
-join.show(50)
-file = "/results/severity_by_hour.csv"
-join.repartition(1).write.mode("overwrite").option("header", "true").option("sep", "|").csv(
-    HDFS_NAMENODE + file)
+
+# CGM VS BGM
+print("-------------------- CGM and BGM --------------------")
+cgm_vs_bgm = df_bgm.join(df_cgm, ["PtID", "Date"], "inner")
+cgm_vs_bgm.show(5)
+
+print("-------------------- CGM VS BGM --------------------")
+cgm_vs_bgm = cgm_vs_bgm.withColumn("DiffInSeconds", abs(col("DateTimeBGM").cast(LongType()) - col("DateTime").cast(LongType())))\
+    .withColumn("DiffInMinutes",round(col("DiffInSeconds")/60))
+cgm_vs_bgm.filter("PtID == 19").orderBy(["DiffInSeconds", "DateTimeBGM"]).show(50)
+cgm_vs_bgm = cgm_vs_bgm.filter("DiffInSeconds < 300")\
+    .withColumn("GlucoseValueDiff", abs(col("GlucoseValue") - col("GlucoseValueBGM")))
+cgm_vs_bgm.show(10)
+cgm_vs_bgm.agg(min("GlucoseValueDiff"), max("GlucoseValueDiff")).show()
+cgm_vs_bgm.orderBy("GlucoseValueDiff", ascending = False).show(50)
+cgm_vs_bgm.orderBy("GlucoseValueDiff", ascending = True).show(50)
+cgm_vs_bgm.orderBy("GlucoseValueDiff").groupBy("GlucoseValueDiff").count().show(250)
+
+bgm_vs_cgm_file = "/results/bgm_vs_cgm.csv"
+cgm_vs_bgm.repartition(1).write.mode("overwrite").option("header", "true").option("sep", "|").csv(
+    HDFS_NAMENODE + bgm_vs_cgm_file)
+# dupes = join_df.groupBy("RecIDBGM").count().filter("count > 1")
+# dupes.join(join_df, "RecIDBGM").orderBy("RecIDBGM").show(50)
--- scripts/regression.py
+++ scripts/regression.py
@@ -36,164 +36,107 @@ df_cgm = spark.read.option("sep", "|").option("header", "true").csv(
 df_cgm = df_cgm.drop("DexInternalDtTmDaysFromEnroll").drop("DexInternalTm").drop("RecordType")
 df_cgm = df_cgm.withColumn("GlucoseValue", col("GlucoseValue").cast(FloatType()))
 
-
-print(df_cgm.count())
 df_cgm.agg(min("GlucoseValue"), max("GlucoseValue")).show()
 
-# df_cgm = df_cgm.withColumn("Year", year(df_cgm["Date"])).withColumn("Month", month(df_cgm["Date"])) \
-#     .withColumn("Day", dayofmonth(df_cgm["Date"]))
-#
-# df_cgm = df_cgm.withColumn("Hour", hour(df_cgm["Time"]))\
-#     .withColumn("Minute", minute(df_cgm["Time"])) \
-#     .withColumn("Second", second(df_cgm["Time"]))
 
-
-# patient1 = df_cgm.filter("PtID == '193'")
-# patient1.show(30)
-# patient1.agg(count("*")).show()
-#
-# seconds = patient1.groupBy("Second").agg(count("*"))
-# seconds.show(truncate=False)
-# df.createOrReplaceTempView("patients")
-
-# cgm_file = "/processed2/HDeviceCGM.csv"
-#
-# df_cgm.partitionBy("Year", "Month").mode("overwrite").option("header", "true").option("sep", "|").csv(
-#     HDFS_NAMENODE + cgm_file)
-
-
-# print("REGRESIJA")
-# # Prepare training data from a list of (label, features) tuples.
-# training = spark.createDataFrame([
-#     (0.0, 0.0),
-#     (1.0, 1.0),
-#     (4.0, 2.0),
-#     (9.0, 3.0),
-#     (16.0, 4.0),
-#     (25.0, 5.0),
-#     (36.0, 6.0),
-#     (49.0, 7.0),
-#     (64.0, 8.0),
-#     (81.0, 9.0),
-#     (144.0, 12.0),
-#     (169.0, 13.0),
-#     (196.0, 14.0),
-#     (225.0, 25.0),
-#     (16.0, 4.0),
-#     (25.0, 5.0),
-#     (36.0, 6.0),
-#     (49.0, 7.0),
-#     (64.0, 8.0),
-#     (81.0, 9.0)
-# ], ["label", "feature"])
-#
-# # Assemble all the features with VectorAssembler
-# required_features = [
-#     # 'Date',
-#     # 'Time',
-#     'Hour',
-#     'Minute',
-#     'Day',
-#     'Month',
-#     'Year'
-# ]
-#
-#
-# patient1 = patient1.limit(30000)
-# dataset = patient1.select(col('Hour').cast('int'),
-#                     col('Minute').cast('int'),
-#                     col('Day').cast('int'),
-#                     col('Month').cast('int'),
-#                     col('Year').cast('int'),
-#                     col('GlucoseValue').cast('float'),
-#                     col('Date'),
-#                     col('Time')
-#             ).filter('GlucoseValue <= 300').filter('GlucoseValue >= 0')
-#
-# assembler = VectorAssembler(inputCols=required_features, outputCol='input')
-# dataset = assembler.transform(dataset)
-# (training_data, test_data) = dataset.randomSplit([0.7, 0.3])
-# # Prepare test data
-# test = spark.createDataFrame([
-#     (121.0, 11.0),
-#     (100.0, 10.0),
-#     (9.0, 3.0),
-#     (2500.0, 50.0)], ["label", "feature"])
-#
-# # test_data = assembler.transform(test)
-# print("TRAIN DATA")
-# training_data.show(5)
-#
-# rf = RandomForestRegressor(labelCol='GlucoseValue',
-#                             featuresCol='input',
-#                             maxDepth=30,
-#                            numTrees=100,
-#                            )
-#
-# model = rf.fit(training_data)
-# predictions = model.transform(test_data)
-#
-# evaluator = RegressionEvaluator(
-#     labelCol='GlucoseValue',
-#     predictionCol='prediction',
-#     metricName='rmse')
-#
-# rmse = evaluator.evaluate(predictions)
-# print("Root Mean Squared Error (RMSE) on test data = %g" % rmse)
-#
-# predictions.select("prediction", "GlucoseValue", "Date", 'Time').show(20)
-
-
-# za predikciju slati 3 prethodna i sledeca
-
-# iskoristiti ono da se pronadje prvi najblizi pa pomocu toga predvidjati
-# koristiti lag, sacuvati datum i vrijeme i vrijednost glukoze u tom trenutku
-
-# windowSpec = Window.partitionBy("PtID").orderBy("Year", "Month", "Day", "Hour", "Minute", "Second")
-
-# def interpol(x, x_prev, x_next, y_prev, y_next, y):
-#     if x_prev == x_next:
-#         return y
-#     else:
-#         m = (y_next - y_prev) / (x_next - x_prev)
-#         y_interpol = y_prev + m * (x - x_prev)
-#         return y_interpol
-#
-#
-# # convert function to udf
-# interpol_udf = func.udf(interpol, FloatType())
-#
-# # add interpolated columns to dataframe and clean up
-# df_filled = df_filled.withColumn('readvalue_interpol',
-#                                  interpol_udf('readtime', 'readtime_ff', 'readtime_bf', 'readvalue_ff', 'readvalue_bf',
-#                                               'readvalue')) \
-#     .drop('readtime_existent', 'readtime_ff', 'readtime_bf') \
-#     .withColumnRenamed('reads_all', 'readvalue') \
-#     .withColumn('readtime', func.from_unixtime(col('readtime')))
-
-
-df_cgm = df_cgm.withColumn("DateTime", concat(col("Date"), lit(' '), col("Time")))\
-    .withColumn("DateTime", to_timestamp(col("DateTime")))
+df_cgm = df_cgm.withColumn("DateTime", concat(col("Date"), lit(' '), col("Time"))) \
+    .withColumn("DateTime", to_timestamp(col("DateTime"))) \
+    .withColumn("GlucoseValue", col("GlucoseValue").cast(FloatType())) \
+    .drop("Date").drop("Time").drop("Day")
 df_cgm.show(5)
 
-
 windowSpec = Window.partitionBy("PtID").orderBy("DateTime")
-# df_cgm.withColumn("row_number", row_number().over(windowSpec)).show()
-# df_cgm.withColumn("dense_rank", dense_rank().over(windowSpec)).show()
 
-# .withColumn("DiffInSeconds",current_timestamp().cast(LongType) -
-#         col("input_timestamp").cast(LongType))
-df_cgm = df_cgm.withColumn("PreviousDateTime", lag("DateTime", 1).over(windowSpec))
-    # .withColumn("PreviousDateTime", to_timestamp(col("PreviousDateTime")))
+# DT - DateTime
+# GV - GlucoseValue
+# P - Previous
+# N - Next
+# S - second
+df_cgm = df_cgm.withColumn("PDT", lag("DateTime", 1).over(windowSpec)) \
+    .withColumn("PGV", lag("GlucoseValue", 1).over(windowSpec)) \
+    .withColumn("SPDT", lag("DateTime", 2).over(windowSpec)) \
+    .withColumn("SPGV", lag("GlucoseValue", 2).over(windowSpec)) \
+    .withColumn("NDT", lag("DateTime", -1).over(windowSpec)) \
+    .withColumn("NGV", lag("GlucoseValue", -1).over(windowSpec)) \
+    .withColumn("SNDT", lag("DateTime", -2).over(windowSpec)) \
+    .withColumn("SNGV", lag("GlucoseValue", -2).over(windowSpec)) \
+    .withColumn("Hour", hour("DateTime")) \
+
+df_filled = df_cgm
+df_cgm = df_cgm.withColumn("PDT", col("DateTime").cast(LongType()) - col("PDT").cast(LongType())) \
+    .withColumn("SPDT", col("DateTime").cast(LongType()) - col("SPDT").cast(LongType())) \
+    .withColumn("NDT", col("NDT").cast(LongType()) - col("DateTime").cast(LongType())) \
+    .withColumn("SNDT", col("SNDT").cast(LongType()) - col("DateTime").cast(LongType()))
+
+df_filled = df_filled.withColumn("PDT", col("PDT").cast(LongType())) \
+    .withColumn("SPDT", col("SPDT").cast(LongType())) \
+    .withColumn("NDT", col("NDT").cast(LongType())) \
+    .withColumn("SNDT", col("SNDT").cast(LongType()))\
+    .withColumn("DateTime", col("DateTime").cast(LongType()))
+
+print("--------------------------------------------------------------------------------------")
+df_cgm = df_cgm.na.drop()
+df_filled = df_filled.na.drop()
 df_cgm.show(10)
-df_cgm = df_cgm.withColumn("DiffInSeconds", col("DateTime").cast(LongType()) - col("PreviousDateTime").cast(LongType()))\
-    .withColumn("DiffInMinutes",round(col("DiffInSeconds")/60))
 
-df_cgm.show(10)
+# # Assemble all the features with VectorAssembler
+required_features = [
+    'Hour',
+    'PGV',
+    'PDT',
+    'SPDT',
+    'SPGV',
+    'NDT',
+    'NGV',
+    'SNDT',
+    'SNGV'
+]
 
-patient1 = df_cgm.filter("PtID == '193'")
+patient1 = df_cgm.filter("PtID == '127'")
+patient1 = patient1.limit(30000)
+df_filled = df_filled.filter("PtID == '127'")
+df_filled = df_filled.limit(30000)
+
+assembler = VectorAssembler(inputCols=required_features, outputCol='input')
+dataset = assembler.transform(patient1)
+(training_data, test_data) = dataset.randomSplit([0.7, 0.3])
+
+print("TRAIN DATA")
+training_data.show(5)
+
+rf = RandomForestRegressor(labelCol='GlucoseValue',
+                           featuresCol='input',
+                           numTrees=50,
+                           )
+
+model = rf.fit(training_data)
+predictions = model.transform(test_data)
+predictions = predictions.withColumn("prediction", round(col("prediction"))) \
+
+evaluator = RegressionEvaluator(
+    labelCol='GlucoseValue',
+    predictionCol='prediction',
+    metricName='rmse')
+
+rmse = evaluator.evaluate(predictions)
+print("Root Mean Squared Error (RMSE) on test data = %g" % rmse)
+
+predictions.select("prediction", "GlucoseValue", "DateTime").show(15)
 
 
+def interpol(x, x_prev, x_next, y_prev, y_next):
+    y_interpol = y_prev + (y_next - y_prev) / (x_next - x_prev) * (x - x_prev)
+    return y_interpol
 
-gbnt = not_using.groupBy("PtID").agg(count("*"))
-gbnt.show()
+
+# convert function to udf
+interpol_udf = func.udf(interpol, FloatType())
+#
+# # add interpolated columns to dataframe and clean up
+(training_data, test_data) = df_filled.randomSplit([0.7, 0.3])
+
+df_filled = test_data.withColumn('prediction', interpol_udf('DateTime', 'PDT', 'NDT', 'PGV', 'NGV'))\
+    .withColumn('prediction', round(col('prediction')))
+rmse = evaluator.evaluate(df_filled)
+print("Interpolation - Root Mean Squared Error (RMSE) on test data = %g" % rmse)
+df_filled.select("prediction", "GlucoseValue", "DateTime").show(15)
