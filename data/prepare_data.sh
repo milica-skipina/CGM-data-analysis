7099a98549be5efe6b944e07346b3baee3456dd7
Milica Škipina <57289672+milica-skipina@users.noreply.github.com>
2021-08-16 17:58:33 +0200

    Update prepare_data.sh

--- data/prepare_data.sh
+++ data/prepare_data.sh
@@ -1,10 +1,6 @@
-docker cp "./Protocol_H/Data Tables/HDeviceCGM.txt" namenode-streaming:/hadoop/dfs/HDeviceCGM.txt
-docker cp "./Protocol_H/Data Tables/HDeviceBGM.txt" namenode-streaming:/hadoop/dfs/HDeviceBGM.txt
-docker cp "./Protocol_H/Data Tables/HScreening.txt" namenode-streaming:/hadoop/dfs/HScreening.txt
-docker cp copy_data.sh namenode-streaming:hadoop/dfs/copy_data.sh
-docker cp ../scripts/ spark-master-streaming:/home
-
-docker exec -it namenode-streaming bash
-# docker exec -it spark-master bash
-
-# /spark/bin/spark-submit home/scripts/preprocess.py
+docker cp "./Protocol_H/Data Tables/HDeviceCGM.txt" namenode:/hadoop/dfs/HDeviceCGM.txt
+docker cp "./Protocol_H/Data Tables/HDeviceBGM.txt" namenode:/hadoop/dfs/HDeviceBGM.txt
+docker cp "./Protocol_H/Data Tables/HScreening.txt" namenode:/hadoop/dfs/HScreening.txt
+docker cp copy_data.sh namenode:hadoop/dfs/copy_data.sh
+docker cp ../scripts/ spark-master:/home
+docker exec -it namenode bash
