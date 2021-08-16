1e54fc330cab83eef40179690c3def4e9bc519e0
Milica Škipina <57289672+milica-skipina@users.noreply.github.com>
2021-08-16 17:59:39 +0200

    Update prepare_data.sh

--- data/prepare_data.sh
+++ data/prepare_data.sh
@@ -1,6 +1,6 @@
-docker cp "./Protocol_H/Data Tables/HDeviceCGM.txt" namenode:/hadoop/dfs/HDeviceCGM.txt
-docker cp "./Protocol_H/Data Tables/HDeviceBGM.txt" namenode:/hadoop/dfs/HDeviceBGM.txt
-docker cp "./Protocol_H/Data Tables/HScreening.txt" namenode:/hadoop/dfs/HScreening.txt
+docker cp "./REPLACE-BG Dataset/Data Tables/HDeviceCGM.txt" namenode:/hadoop/dfs/HDeviceCGM.txt
+docker cp "./REPLACE-BG Dataset/Data Tables/HDeviceBGM.txt" namenode:/hadoop/dfs/HDeviceBGM.txt
+docker cp "./REPLACE-BG Dataset/Data Tables/HScreening.txt" namenode:/hadoop/dfs/HScreening.txt
 docker cp copy_data.sh namenode:hadoop/dfs/copy_data.sh
 docker cp ../scripts/ spark-master:/home
 docker exec -it namenode bash
